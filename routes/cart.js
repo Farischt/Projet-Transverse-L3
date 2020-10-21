b2e413ad35177af834f720f592670fa62b29bcf8
Faris Chtatou <f.chtatou@hotmail.fr>
2020-10-21 18:14:13 +0200

    first get and post routes

--- routes/cart.js
+++ routes/cart.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const Item = require('../model/Items');
 const { verifyAuth }   = require('./verifyAuth');
 
 class Cart {
@@ -15,16 +16,40 @@ router.use((req, res, next) => {
     if (req.session.userId && typeof req.session.cart === "undefined") {
         req.session.cart = new Cart();
         next();
-    } 
-    res.status(401).send('Acces Denied')
+    }  
+    else {
+        next();
+    }
 })
 
-router.get('/test', (req, res) => {
-    res.send('GET REQUEST 1 ')
+router.get('/cart', verifyAuth, (req, res) => {
+    res.send(req.session.cart)   // a faire avec la db
 })
 
-router.get('/testt', (req, res) => {
-    res.send('GET REQUEST 2')
+router.post('/additem', verifyAuth, async (req, res) => {
+    const item = new Item({
+        name: req.body.name,
+        description: req.body.descritpion,
+        price: parseInt(req.body.price),
+    })
+
+    try {
+        const savedItem = item.save();
+        console.log(savedItem._id);
+        res.send({ item: savedItem._id});
+    } catch(error) {
+        res.status(400).send(error);
+    }
 })
+
+/*router.post('/cart', async (req, res) => {
+    // We check if the item exist in dataBase
+    const item = await findOne({ _id: parseInt(req.body.id)});
+    if(!item) return res.status(404).send(`Article number ${req.body.id} doesn't.`);
+
+    // We add the item in cart
+    req.session.cart.push({ id: parseInt(req.body.id), quantity: parseInt(req.body.quantity) });
+    return res.send(req.session.carte)
+})*/
 
 module.exports = router;
