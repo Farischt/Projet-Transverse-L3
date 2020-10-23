1c1806aa17afdf8494efda58570e43514747ab50
Faris Chtatou <f.chtatou@hotmail.fr>
2020-10-23 13:51:30 +0200

    new route to get items from db

--- routes/cart.js
+++ routes/cart.js
@@ -26,16 +26,26 @@ router.get('/cart', verifyAuth, (req, res) => {
     res.send(req.session.cart)   // a faire avec la db
 })
 
+router.get('/items', verifyAuth, async (req, res) => {
+    try {
+        // We request the db to get all the items
+        const items = await Item.find({})
+        if(items.length === 0) return res.status(404).send('No available data, please add some items first')
+        res.send(items)
+    } catch(err) {
+        res.status(400).send(error)
+    }
+})
+
 router.post('/additem', verifyAuth, async (req, res) => {
     const item = new Item({
         name: req.body.name,
         description: req.body.descritpion,
         price: parseInt(req.body.price),
     })
-
     try {
-        const savedItem = item.save();
-        console.log(savedItem._id);
+        const savedItem = await item.save();
+        console.log(savedItem);
         res.send({ item: savedItem._id});
     } catch(error) {
         res.status(400).send(error);
