da2ab9e6bd96b671bf7de218ff5d15ea7850aa5c
Faris Chtatou <f.chtatou@hotmail.fr>
2020-10-30 00:42:49 +0100

    basic route to query a specific item

--- server/routes/item.js
+++ server/routes/item.js
@@ -5,7 +5,7 @@ const { verifyAuth }   = require('../helpers/verifyAuth');
 
 // this route return all the items in items table (not the cart)
 
-router.get('/',/* verifyAuth,*/ async (req, res) => {
+router.get('/', async (req, res) => {
     try {
         // We request the db to get all the items
         const items = await Item.find({})
@@ -16,7 +16,7 @@ router.get('/',/* verifyAuth,*/ async (req, res) => {
     }
 })
 
-// This route add an item to items Table Only
+// This route add an item to items Table Only : Admin part 
 
 router.post('/', verifyAuth, async (req, res) => {
     const item = new Item({
@@ -30,6 +30,18 @@ router.post('/', verifyAuth, async (req, res) => {
     } catch(error) {
         res.status(400).json({ errorMessage: error });
     }
+})
+
+// This route return specific items based on the query. Search function 
+router.get('/:query', async (req, res) => {
+    // We first check that the query is a string
+    if(typeof(req.params.query) != 'string') return res.status(400).json({ errorMessage: "Bad request" })
+
+    // Then we ask the db for a list of items matching the query 
+    const items = await Item.find({ name: req.params.query })
+    if(items.length === 0) return res.status(404).json({ errorMessage: "Not found, no item matching with your query" })
+
+    res.json(items)
 })
 
 module.exports = router;
