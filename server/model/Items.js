79f66ac0d6552c5acb763a9439430eb7cb36e434
Faris Chtatou <f.chtatou@hotmail.fr>
2020-10-31 18:03:58 +0100

    likes key added

--- server/model/Items.js
+++ server/model/Items.js
@@ -1,24 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const itemSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        min: 2,
-        max: 255,
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
-})
+  name: {
+    type: String,
+    required: true,
+    min: 2,
+    max: 255,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  sales: {
+    type: Number,
+    default: 0,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-module.exports = mongoose.model('Item', itemSchema)
+module.exports = mongoose.model("Item", itemSchema);
