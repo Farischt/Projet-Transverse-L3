b36878a62b64170f56cf3f951da5aa9be4de43e0
Faris Chtatou <f.chtatou@hotmail.fr>
2020-10-31 18:01:40 +0100

    Cleaned code + some css

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,33 +1,44 @@
-import React, { Component } from 'react'
-import Login from './Login'
-import { Link } from 'react-router-dom'
-import { Navbar, Nav } from 'react-bootstrap';
-
+import React, { Component } from "react";
+import Login from "./Login";
+import { Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
 
 class NavBar extends Component {
-    render() {
-        return (        
-        <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Brand >e-Commerce</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                   <Link to="/" className="nav-link" > Accueil </Link>
-                </Nav>
-                <Nav className="mr-auto">
-                   <Link to="/register" className="nav-link" > S'inscrire </Link>
-                </Nav>
-                <Nav className="mr-auto">
-                   <Link to="/profil" className="nav-link" > Mon profil </Link>
-                </Nav>
-                <Nav className="mr-auto">
-                   <Link to="/cart" className="nav-link" > <i className="large shopping cart icon"></i> </Link>
-                </Nav>
-                <Login login={this.props.login} userName={this.props.userName}/>
-            </Navbar.Collapse>
-        </Navbar>
-        )
-    }
+  render() {
+    return (
+      <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand>e-Commerce</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Link to="/" className="nav-link">
+              {" "}
+              Accueil{" "}
+            </Link>
+          </Nav>
+          <Nav className="mr-auto">
+            <Link to="/register" className="nav-link">
+              {" "}
+              S'inscrire{" "}
+            </Link>
+          </Nav>
+          <Nav className="mr-auto">
+            <Link to="/profil" className="nav-link">
+              {" "}
+              Mon profil{" "}
+            </Link>
+          </Nav>
+          <Nav className="mr-auto">
+            <Link to="/cart" className="nav-link">
+              {" "}
+              <i className="large shopping cart icon"></i>{" "}
+            </Link>
+          </Nav>
+          <Login login={this.props.login} userName={this.props.userName} />
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 }
 
 export default NavBar;
--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,26 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
+class SearchBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      query: "",
+    };
+  }
 
-class SearchBar extends Component{
-    constructor() {
-        super()
-        this.state = {
-            query: "",
-        }
-    }
+  handleInputChange = (event) => {
+    this.setState({ query: event.target.value });
+  };
 
-    handleInputChange = (event) => {
-        this.setState({ query: event.target.value })
-    }
-
-    render() {
-        return ( 
-                <form className="form-inline">
-                    <input onChange={this.handleInputChange} className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit" style={{backgroundColor: "#353a40"}}>Search</button>
-                </form>
-        )
-    }
+  render() {
+    return (
+      <form
+        className="form-inline justify-content-center"
+        style={{ backgroundColor: "#17a2b7", margin: "5vh" }}
+      >
+        <input
+          style={{ width: "70%" }}
+          onChange={this.handleInputChange}
+          className="form-control mr-sm-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button
+          className="btn btn-outline-success my-2 my-sm-0"
+          type="submit"
+          style={{ backgroundColor: "#353a40" }}
+        >
+          Search
+        </button>
+      </form>
+    );
+  }
 }
 
 export default SearchBar;
