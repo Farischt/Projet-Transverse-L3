c32c98ee45156ee99171150ce1c61929f43aa65a
Faris Chtatou <f.chtatou@hotmail.fr>
2020-10-31 18:03:10 +0100

    cleaned code + error status fixed

--- server/index.js
+++ server/index.js
@@ -1,37 +1,46 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const authRoute = require("./routes/auth");
-const cartRoute = require('./routes/cart');
-const itemRoute = require('./routes/item');
-require('dotenv').config();
+const cartRoute = require("./routes/cart");
+const itemRoute = require("./routes/item");
+require("dotenv").config();
 
 const app = express();
 
-//DB Connect 
-mongoose.connect(process.env.DB_CONNECT, { 
-        useNewUrlParser: true, 
-        useUnifiedTopology: true,
-    },
-    () => {
+//DB Connect
+mongoose.connect(
+  process.env.DB_CONNECT,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+  () => {
     console.log("connected to db");
-})
+  }
+);
 
 //Middlewares
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser()) 
-app.use(session({ secret: process.env.SESSION_SECRET, saveUninitialized: false, resave: false }));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+  })
+);
 
+// Route Middlewares :
+app.use("/api/user", authRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/item", itemRoute);
 
-// Route Middlewares : 
-app.use('/api/user', authRoute);
-app.use('/api/cart', cartRoute);
-app.use('/api/item', itemRoute);
-
-app.listen(3080, () => console.log('Server up and running'));
+app.listen(3080, () => console.log("Server up and running"));
--- server/routes/cart.js
+++ server/routes/cart.js
@@ -1,98 +1,113 @@
-const router = require('express').Router();
-const Item = require('../model/Items');
-const { verifyAuth }   = require('../helpers/verifyAuth');
-const mongoose = require('mongoose');
+const router = require("express").Router();
+const Item = require("../model/Items");
+const { verifyAuth } = require("../helpers/verifyAuth");
+const mongoose = require("mongoose");
 const ObjectId = mongoose.Types.ObjectId;
 
 class Cart {
-    constructor() {
-      this.createdAt = new Date();
-      this.updatedAt = new Date();
-      this.items = [];
-      this.userId = null
-    }
+  constructor() {
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+    this.items = [];
+    this.userId = null;
   }
+}
 
 // Middleware creating a cart for the session
 router.use((req, res, next) => {
-    if (req.session.userId && typeof req.session.cart === "undefined") {
-        req.session.cart = new Cart();
-        next();
-    }  
-    else {
-        next();
-    }
-})
+  if (req.session.userId && typeof req.session.cart === "undefined") {
+    req.session.cart = new Cart();
+    next();
+  } else {
+    next();
+  }
+});
 
 // Get cart
-router.get('/', verifyAuth, (req, res) => {
-    res.json(req.session.cart)   
-})
+router.get("/", verifyAuth, (req, res) => {
+  res.json(req.session.cart);
+});
 
 // Add an item in cart
-router.post('/:_id', verifyAuth, async (req, res) => {
-    // We first check if the id is a mongoose.Types.ObjectId
-    if(!ObjectId.isValid(req.params._id)) return res.status(404).send("item's id isn't valid")
-    
-    // We then check if the quantity is a number
-    if(isNaN(parseInt(req.body.quantity)) || parseInt(req.body.quantity) <= 0) return res.status(404).send('Quantity must be a number')
+router.post("/:_id", verifyAuth, async (req, res) => {
+  // We first check if the id is a mongoose.Types.ObjectId
+  if (!ObjectId.isValid(req.params._id))
+    return res.status(400).send("item's id isn't valid");
 
-    // We need to check if the item in req.params is in the database
-    const item = await Item.findById(req.params._id);      
-    if(!item) return res.status(404).send('Item not found in db')
+  // We then check if the quantity is a number
+  if (isNaN(parseInt(req.body.quantity)) || parseInt(req.body.quantity) <= 0)
+    return res.status(400).send("Quantity must be a number");
 
-    // Then we need to check if the article is already in the cart 
-    const cart = req.session.cart.items.find(element => element._id === req.params._id)
-    if(cart) return res.status(400).send('Item is already in cart, delete it, or change quantity in the cart')
+  // We need to check if the item in req.params is in the database
+  const item = await Item.findById(req.params._id);
+  if (!item) return res.status(404).send("Item not found in db");
 
-    // Now we can update our  cart 
-    req.session.cart.items.push({ 
-        _id: req.params._id, 
-        quantity: parseInt(req.body.quantity)
-    })
-    req.session.cart.updatedAt = new Date()
-    req.session.cart.userId = req.session.userId
-    res.send(req.session.cart)
-})
+  // Then we need to check if the article is already in the cart
+  const cart = req.session.cart.items.find(
+    (element) => element._id === req.params._id
+  );
+  if (cart)
+    return res
+      .status(400)
+      .send(
+        "Item is already in cart, delete it, or change quantity in the cart"
+      );
+
+  // Now we can update our  cart
+  req.session.cart.items.push({
+    _id: req.params._id,
+    quantity: parseInt(req.body.quantity),
+  });
+  req.session.cart.updatedAt = new Date();
+  req.session.cart.userId = req.session.userId;
+  res.send(req.session.cart);
+});
 
 // Change the quantity of an item inside the cart
-router.put('/:_id', verifyAuth, async (req, res) => {
-    // We first check if the id is a mongoose.types.ObjectId in order to handle errors
-    if(!ObjectId.isValid(req.params._id)) return res.status(404).send("item's id isn't valid")
+router.put("/:_id", verifyAuth, async (req, res) => {
+  // We first check if the id is a mongoose.types.ObjectId in order to handle errors
+  if (!ObjectId.isValid(req.params._id))
+    return res.status(400).send("item's id isn't valid");
 
-    // We then check if the quantity is a number
-    if(isNaN(parseInt(req.body.quantity)) || parseInt(req.body.quantity) <= 0) return res.status(404).send('Quantity must be a number')
+  // We then check if the quantity is a number
+  if (isNaN(parseInt(req.body.quantity)) || parseInt(req.body.quantity) <= 0)
+    return res.status(400).send("Quantity must be a number");
 
-    // We need to check if the item in req.params is in the database
-    const item = await Item.findById(req.params._id);      
-    if(!item) return res.status(404).send('Item not found in db')
+  // We need to check if the item in req.params is in the database
+  const item = await Item.findById(req.params._id);
+  if (!item) return res.status(404).send("Item not found in db");
 
-    // We then check if the item is in the cart.
-    const itemInCart = req.session.cart.items.find((element) => element._id === req.params._id)
-    if(!itemInCart) return res.status(404).send('Item not found in cart')
+  // We then check if the item is in the cart.
+  const itemInCart = req.session.cart.items.find(
+    (element) => element._id === req.params._id
+  );
+  if (!itemInCart) return res.status(404).send("Item not found in cart");
 
-    // We finally can change the quantity 
-    itemInCart.quantity = parseInt(req.body.quantity)
-    res.json(req.session.cart)
-})
+  // We finally can change the quantity
+  itemInCart.quantity = parseInt(req.body.quantity);
+  res.json(req.session.cart);
+});
 
 // Delete an item from the cart
-router.delete('/:_id', verifyAuth, async (req, res) => {
-    // We first check if the id is a mongoose.Types.ObjectId
-    if(!ObjectId.isValid(req.params._id)) return res.status(404).send("item's id isn't valid")
+router.delete("/:_id", verifyAuth, async (req, res) => {
+  // We first check if the id is a mongoose.Types.ObjectId
+  if (!ObjectId.isValid(req.params._id))
+    return res.status(400).send("item's id isn't valid");
 
-    // We need to check if the item in req.params is in the database
-    const item = await Item.findById(req.params._id);      
-    if(!item) return res.status(404).send('Item not found in db')
+  // We need to check if the item in req.params is in the database
+  const item = await Item.findById(req.params._id);
+  if (!item) return res.status(404).send("Item not found in db");
 
-    // We then check if the item is in the cart.
-    const itemInCart = req.session.cart.items.findIndex((element) => element._id === req.params._id)
-    if(itemInCart === -1) return res.status(404).send('Item not found in cart')
+  // We then check if the item is in the cart.
+  const itemInCart = req.session.cart.items.findIndex(
+    (element) => element._id === req.params._id
+  );
+  if (itemInCart === -1) return res.status(404).send("Item not found in cart");
 
-    // Otherwise we delete it
-    req.session.cart.items.splice(itemInCart, 1)
-    req.session.cart.updatedAt = new Date()
-    res.json(req.session.cart)
-})
+  // Otherwise we delete it
+  req.session.cart.items.splice(itemInCart, 1);
+  req.session.cart.updatedAt = new Date();
+  res.json(req.session.cart);
+});
 
 module.exports = router;
