e85646083c7bfdd15cda75427638f0db7d6b5fed
Faris Chtatou <f.chtatou@hotmail.fr>
2020-11-11 22:30:01 +0100

    new admin route, issue here

--- client/src/routes/AdminRoute.jsx
+++ client/src/routes/AdminRoute.jsx
@@ -1,30 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
-import axios from "axios";
 
 const UserRoute = ({ component: Component, ...rest }) => {
   const { user } = useSelector((state) => ({ ...state }));
-  const { ok, setOk } = useState(false);
-
-  useEffect(() => {
-    const getCurrentUser = async () => {
-      if (user && userIsLoggedIn) {
-        try {
-          await axios.get("/api/user/admin");
-          setOk(true);
-        } catch (err) {
-          setOk(false);
-        }
-      }
-    };
-    getCurrentUser();
-  }, [user]);
 
   return (
     <Route
       {...rest}
-      render={(props) => (ok ? <Component {...props} /> : <Redirect to="/" />)}
+      render={(props) =>
+        user && user.role === "admin" ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/" />
+        )
+      }
     />
   );
 };
