f717656bda463793fd632ab57491c5f5b0cdb5b7
Faris Chtatou <f.chtatou@hotmail.fr>
2020-11-12 14:57:34 +0100

    cleaned code

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -17,19 +17,14 @@ const {
 
 // Register route that create a user in DB
 router.post("/register", register);
-
 // Login route that connect the user
 router.post("/login", verifyConnection, login);
-
 // Logout route
 router.get("/logout", verifyAuth, logout);
-
 // Current user route
 router.get("/me", verifyAuth, currentUser);
-
 // Current admin route
 router.get("/admin", verifyAuth, verifyAdmin, currentUser);
-
 // Update password route
 router.put("/password/:_id", verifyAuth, updatePassword);
 
