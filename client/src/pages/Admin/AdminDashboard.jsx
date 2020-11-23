d42c3243f8634287cf633ada9753e704ef0c9e1c
Faris Chtatou <f.chtatou@hotmail.fr>
2020-11-23 21:34:01 +0100

    admin dashboard created

--- client/src/pages/Admin/AdminDashboard.jsx
+++ client/src/pages/Admin/AdminDashboard.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+import AdminNav from "../../components/Nav/AdminNav";
+
 const AdminDashboard = () => {
-  return <h1> Admin dashboard </h1>;
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          {" "}
+          <AdminNav />{" "}
+        </div>
+        <div className="col">
+          <h1> Dashboard </h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminDashboard;
