3408112bc7ca5bf7527c4e9fc19d3835d1045797
Faris Chtatou <f.chtatou@hotmail.fr>
2020-11-23 21:33:05 +0100

    admin dashboard add category form

---
+++ client/src/pages/Admin/Category/CategoryCreate.jsx
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from "react";
+import useSelector from "react-redux";
+import { toast } from "react-toastify";
+import {
+  getCategories,
+  getCategory,
+  removeCategory,
+  updateCategory,
+  createCategory,
+} from "../../../api/category";
+import AdminNav from "../../../components/Nav/AdminNav";
+
+const CategoryCreate = () => {
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const categoryForm = () => {
+    return (
+      <div className="form">
+        <label> Nom </label>
+        <input
+          onChange={handleChange}
+          className="form-control my-2"
+          type="text"
+          value={name}
+          autoFocus
+          required
+        />
+        <button onClick={handleClick} className="btn btn-info my-2 my-sm-0">
+          {" "}
+          Enregister{" "}
+        </button>
+      </div>
+    );
+  };
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    createCategory({ name })
+      .then((res) => {
+        setLoading(false);
+        setName("");
+        toast.succes(`La catégorie ${res.data.name} a été ajouté`);
+      })
+      .catch((error) => {
+        setLoading(false);
+        if (error.response.status === 400)
+          toast.error(error.response.data.errorMessage);
+        else toast.error("Une erreur est survenu");
+      });
+  };
+
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          {" "}
+          <AdminNav />{" "}
+        </div>
+        <div className="col">
+          <h1> Créer une catégorie </h1>
+          {categoryForm()}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryCreate;
