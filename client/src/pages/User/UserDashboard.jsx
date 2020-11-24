651bbd4e83a496e076c60a77e92b4fa22a8753b0
Faris Chtatou <f.chtatou@hotmail.fr>
2020-11-24 19:20:58 +0100

    due to User page simplification

--- client/src/pages/User/UserDashboard.jsx
+++ client/src/pages/User/UserDashboard.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import { Switch, Route, useRouteMatch } from "react-router-dom";
 import UserNav from "../../components/UserNav";
+import Password from "./components/Password";
 
 const UserDashboard = () => {
+  let { path, url } = useRouteMatch();
+
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="col-md-2">
+        <div className="col-md-2" style={{ backgroundColor: "red" }}>
           {" "}
-          <UserNav />{" "}
+          <UserNav url={url} />{" "}
         </div>
         <div className="col">
-          <h1> Dashboard </h1>
+          <Switch>
+            <Route path={`${path}`} exact>
+              Votre Espace Personnel
+            </Route>
+            <Route component={Password} path={`${path}/password`} />
+          </Switch>
         </div>
       </div>
     </div>
