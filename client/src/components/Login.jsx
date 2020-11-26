d97fa03e96173f39f19de9712fb4f55329e05190
Faris Chtatou <f.chtatou@hotmail.fr>
2020-11-26 18:41:47 +0100

    refactored and cleaned code

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,57 +1,32 @@
-import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { toast } from "react-toastify";
-import axios from "axios";
+import React, { useState } from "react";
+// import { useHistory } from "react-router-dom";
+//import { toast } from "react-toastify";
 import "../css/Buttons.css";
 import { connect } from "react-redux";
 import { loginUser } from "../redux";
 
-const Login = ({ userData, connected, loginUser }) => {
-  let dispatch = useDispatch();
-  let history = useHistory();
+const Login = ({ userData, loginUser }) => {
+  // let history = useHistory();
 
-  const [userr, setUserr] = useState({
+  const [user, setUser] = useState({
     email: "",
     password: "",
   });
 
   const handleInputChange = (event) => {
     const userInfo = {
-      ...userr,
+      ...user,
       [event.target.name]: event.target.value,
     };
-    setUserr(() => userInfo);
+    setUser(() => userInfo);
   };
 
-  const roleRedirect = (res) => {
-    if (res.data.userRole === "admin") history.push("/admin/dashboard");
-  };
-
-  // const handleLogin = async () => {
-  //   try {
-  //     const login = await axios.post("/api/user/login", userr);
-  //     const getUser = await axios.get("/api/user/me");
-  //     const userLikes = await axios.get("/api/item/liked");
-  //     dispatch({
-  //       type: "LOGGED_IN_USER",
-  //       payload: {
-  //         name: getUser.data.userName,
-  //         role: getUser.data.userRole,
-  //         _id: login.data.user,
-  //         isLoggedIn: true,
-  //         likedItems: userLikes.data,
-  //       },
-  //     });
-  //     roleRedirect(getUser);
-  //   } catch (err) {
-  //     console.log(err);
-  //     toast.error(" Une erreur est survenu ");
-  //   }
+  // const roleRedirect = (res) => {
+  //   if (res.data.userRole === "admin") history.push("/admin/dashboard");
   // };
 
-  const handleClick = () => {
-    loginUser(userr);
+  const handleLogin = () => {
+    loginUser(user);
   };
 
   return (
@@ -71,10 +46,7 @@ const Login = ({ userData, connected, loginUser }) => {
           name="password"
           placeholder="Mot de passe"
         />
-        {/* <button onClick={handleLogin} className="btn btn-primary my-2 my-sm-0">
-          Se connecter
-        </button> */}
-        <button onClick={handleClick} className="btn btn-primary my-2 my-sm-0">
+        <button onClick={handleLogin} className="btn btn-primary my-2 my-sm-0">
           Se connecter
         </button>
       </div>
@@ -85,7 +57,6 @@ const Login = ({ userData, connected, loginUser }) => {
 const mapStateToProps = (state) => {
   return {
     userData: state.user,
-    connected: state.isLoggedIn,
   };
 };
 
