537fce2359c2ba1b377b791f858a8c1b967ea3ec
Faris Chtatou <f.chtatou@hotmail.fr>
2020-11-26 18:34:20 +0100

    deleted previous reducer since it was not async

--- client/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { userReducer } from "./userReducer";
-import { userThunkReducer } from "../redux/user/userReducer";
-
-const rootReducer = combineReducers({
-  user: userThunkReducer,
-});
-
-export default rootReducer;
