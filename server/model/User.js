3b420efbc24e7f9a8d0fd750f7d7d0b87924f76e
Faris Chtatou <f.chtatou@hotmail.fr>
2020-11-27 09:03:55 +0100

    added a trim to user schema (name key)

--- server/model/User.js
+++ server/model/User.js
@@ -1,31 +1,33 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    min: 2,
-    max: 255,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 255,
+      trim: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    email: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 255,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 1024,
+      trim: true,
+    },
   },
-  role: {
-    type: String,
-    default: "user",
-  },
-  email: {
-    type: String,
-    required: true,
-    min: 6,
-    max: 255,
-  },
-  password: {
-    type: String,
-    required: true,
-    min: 6,
-    max: 1024,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 module.exports = mongoose.model("User", userSchema);
