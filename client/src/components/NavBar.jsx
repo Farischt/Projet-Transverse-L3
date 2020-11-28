f3f6b08d6ea3c6b3463d7ffc3bad49b3c1dd318b
Faris Chtatou <f.chtatou@hotmail.fr>
2020-11-28 01:44:01 +0100

    css modules for navbar, test

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,10 +3,9 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
 
+import styles from "../css/NavBar.module.css";
 import Login from "./Login";
 import Logout from "./Logout";
-
-import "../css/NavBar.css";
 
 const NavBar = () => {
   let { user } = useSelector((state) => ({
@@ -15,12 +14,11 @@ const NavBar = () => {
 
   return (
     <Navbar
-      className="color-nav"
+      className={styles.nav}
       fixed="top"
       collapseOnSelect
       expand="xl"
-      bg="dark"
-      variant="dark"
+      variant="light"
     >
       <Navbar.Brand>Formations & Logiciels</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -68,10 +66,7 @@ const NavBar = () => {
           <Nav className="mr-auto">
             <Link to="/admin/dashboard">
               {" "}
-              <button className="btn btn-primary my-2 my-sm-0">
-                {" "}
-                <i className="user outline icon"></i>Admin
-              </button>
+              <button className="btn btn-primary my-2 my-sm-0"> Admin</button>
             </Link>
           </Nav>
         )}
@@ -80,8 +75,7 @@ const NavBar = () => {
             <Nav className="mr-auto">
               <Link to="/user/dashboard" className="nav-link">
                 <button className="btn btn-primary my-2 my-sm-0">
-                  {" "}
-                  <i className="user outline icon"></i>Mon profil
+                  Profile
                 </button>
               </Link>
             </Nav>
@@ -95,52 +89,6 @@ const NavBar = () => {
             <div className="ui massive text loader">Chargement...</div>
           </div>
         )}
-
-        {/* {user && user.loading ? (
-          <Nav className="mr-auto">
-            <div className="ui active dimmer">
-              <div className="ui massive text loader">Chargement...</div>
-            </div>
-          </Nav>
-        ) : user && user.isLoggedIn ? (
-          <>
-            <Nav className="mr-auto">
-              <Link to="/user/dashboard" className="nav-link">
-                <button className="btn btn-primary my-2 my-sm-0">
-                  {" "}
-                  <i className="user outline icon"></i>Mon profil
-                </button>
-              </Link>
-            </Nav>
-            <Nav className="mr-auto">
-              <Logout />
-            </Nav>
-          </>
-        ) : user && user.isLoggedIn && user.user.userRole === "admin" ? (
-          <Nav className="mr-auto">
-            <Link to="/admin/dashboard">
-              {" "}
-              <button className="btn btn-primary my-2 my-sm-0">
-                {" "}
-                <i className="user outline icon"></i>Admin
-              </button>
-            </Link>
-          </Nav>
-        ) : (
-          <>
-            <Nav className="mr-auto">
-              <Login />
-            </Nav>
-            <Nav className="mr-auto">
-              <Link to="/register" className="nav-link">
-                <button className="btn btn-primary my-2 my-sm-0">
-                  {" "}
-                  S'inscrire{" "}
-                </button>
-              </Link>
-            </Nav>
-          </>
-        )} */}
       </Navbar.Collapse>
     </Navbar>
   );
