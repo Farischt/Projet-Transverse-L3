9c5146c5c837c74b0fd579a5276c9786beddc98d
Faris Chtatou <faris.manage@gmail.com>
2020-11-30 12:44:36 +0100

    new route to remove images

--- server/routes/cloudinary.js
+++ server/routes/cloudinary.js
@@ -2,8 +2,9 @@
 const router = require("express").Router();
 // Middlewares
 const { verifyAuth, verifyAdmin } = require("../middlewares/auth");
-const { upload } = require("../controller/cloudinaryController");
+const { upload, remove } = require("../controller/cloudinaryController");
 
 router.post("/uploadimages", verifyAuth, verifyAdmin, upload);
+router.post("/removeimage", verifyAuth, verifyAdmin, remove);
 
 module.exports = router;
