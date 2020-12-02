51af30e556d8fa011ea542c6e9b5e86136ae1289
Faris Chtatou <faris.manage@gmail.com>
2020-12-03 00:59:05 +0100

    called new api routes to fetch products using specific criteras

--- client/src/api/product.js
+++ client/src/api/product.js
@@ -11,3 +11,27 @@ export const listProducts = async () => {
 export const removeProduct = async (slug) => {
   return await axios.delete(`${process.env.REACT_APP_API_URL}/product/${slug}`);
 };
+
+export const listSpecificProducts = async (sort, order, limit) => {
+  return await axios.post(`${process.env.REACT_APP_API_URL}/products`, {
+    sort,
+    order,
+    limit,
+  });
+};
+
+export const listPagination = async (sort, order, page) => {
+  return await axios.post(`${process.env.REACT_APP_API_URL}/products/page`, {
+    sort,
+    order,
+    page,
+  });
+};
+
+export const productsTotal = async () => {
+  return await axios.get(`${process.env.REACT_APP_API_URL}/products/total`);
+};
+
+export const readProduct = async (slug) => {
+  return await axios.get(`${process.env.REACT_APP_API_URL}/product/${slug}`);
+};
