f7673019c23df0d6000bdeb7fee70c235a98ae6d
Faris Chtatou <faris.manage@gmail.com>
2020-12-07 01:19:13 +0100

    deleted useless import statments

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import ReactGa from "react-ga";
 import Jumbotron from "./components/Jumbotron";
 import NewArrivals from "./components/NewArrivals";
