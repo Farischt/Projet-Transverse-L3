a0f9ba918e49426bb5354faa0b92a5841c17e198
Faris Chtatou <faris.manage@gmail.com>
2020-12-10 00:06:27 +0100

    displayed related products based on category

--- client/src/pages/Product/ProductView.jsx
+++ client/src/pages/Product/ProductView.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
+import ReactGa from "react-ga";
 import Spinner from "react-bootstrap/Spinner";
 import SingleProduct from "./SingleProduct";
-import { readProduct } from "../../api/product";
+import ProductCard from "../Home/components/ProductCard";
+import { readProduct, listRelated } from "../../api/product";
+import { CardColumns } from "react-bootstrap";
 
 const ProductView = ({ match }) => {
   const [product, setProduct] = useState({});
   const [loading, setLoading] = useState(false);
+  const [related, setRelated] = useState([]);
   const { slug } = match.params;
 
   useEffect(() => {
@@ -13,8 +17,10 @@ const ProductView = ({ match }) => {
       setLoading(true);
       try {
         const response = await readProduct(slug);
-        setLoading(false);
         setProduct(response.data);
+        const relatedResponse = await listRelated(response.data._id);
+        setRelated(relatedResponse.data);
+        setLoading(false);
       } catch (err) {
         console.log(err);
         setLoading(false);
@@ -22,6 +28,10 @@ const ProductView = ({ match }) => {
     };
     getProduct();
   }, [slug]);
+
+  useEffect(() => {
+    ReactGa.pageview(window.location.pathname + window.location.search);
+  });
 
   return (
     <div className="container-fluid">
@@ -32,9 +42,32 @@ const ProductView = ({ match }) => {
           <span className="text-info"> Chargement ... </span>
         </div>
       ) : (
-        <div className="row pt-4">
-          <SingleProduct product={product} />
-        </div>
+        <>
+          <div className="row pt-4">
+            <SingleProduct product={product} />
+          </div>
+          <hr />
+          <div className="row p-2">
+            {product && product._id && related && related.length ? (
+              <>
+                <div className="col text-center display-4 font-weight-bold">
+                  {" "}
+                  Produits similaires{" "}
+                </div>
+                <CardColumns className="p-2">
+                  {related.map((element) => {
+                    return <ProductCard key={element._id} product={element} />;
+                  })}
+                </CardColumns>
+              </>
+            ) : (
+              <div className="col text-center display-4 font-weight-bold">
+                {" "}
+                Aucun produits similaires{" "}
+              </div>
+            )}
+          </div>
+        </>
       )}
     </div>
   );
