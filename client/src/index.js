6badaf21f4a3e5aec69eea5ebe4c2e71179cd9fb
Faris Chtatou <faris.manage@gmail.com>
2020-12-09 01:14:27 +0100

    moved css import statments in top level

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -22,7 +24,6 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </Provider>,
-
   document.getElementById("root")
 );
 
