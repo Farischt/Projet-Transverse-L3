e525ec36296d92bd350b78355c0a2d4da5f46de7
Faris Chtatou <faris.manage@gmail.com>
2020-12-09 01:20:41 +0100

    useEffect refactored (es-lint warning) + loading system

--- client/src/pages/Product/ProductView.jsx
+++ client/src/pages/Product/ProductView.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
-
+import Spinner from "react-bootstrap/Spinner";
 import SingleProduct from "./SingleProduct";
-
 import { readProduct } from "../../api/product";
 
 const ProductView = ({ match }) => {
@@ -10,28 +9,33 @@ const ProductView = ({ match }) => {
   const { slug } = match.params;
 
   useEffect(() => {
+    const getProduct = async () => {
+      setLoading(true);
+      try {
+        const response = await readProduct(slug);
+        setLoading(false);
+        setProduct(response.data);
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+      }
+    };
     getProduct();
   }, [slug]);
 
-  const getProduct = async () => {
-    try {
-      setLoading(true);
-      const response = await readProduct(slug);
-      setLoading(false);
-      setProduct(response.data);
-    } catch (err) {
-      console.log(err);
-      setLoading(false);
-    }
-  };
-
   return (
     <div className="container-fluid">
-      <div className="row pt-4">
-        <SingleProduct product={product} />
-      </div>
-
-      <div className="row"></div>
+      {loading ? (
+        <div className="text-center" style={{ marginTop: "40vh" }}>
+          <Spinner animation="border" variant="info" role="status" as="span" />
+          <br />
+          <span className="text-info"> Chargement ... </span>
+        </div>
+      ) : (
+        <div className="row pt-4">
+          <SingleProduct product={product} />
+        </div>
+      )}
     </div>
   );
 };
