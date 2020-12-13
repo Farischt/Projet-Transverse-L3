d95fe75663aec27e796c01be3f010958f590d679
Faris Chtatou <faris.manage@gmail.com>
2020-12-13 01:05:16 +0100

    deleted a tags

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -12,76 +12,40 @@ const Footer = () => {
         </div>
         <div className="col-6 col-md">
           <h5 className="text-white">Features</h5>
-          <ul className="list-unstyled text-small">
-            <li>
-              <a className="text-muted">Cool stuff</a>
-            </li>
-            <li>
-              <a className="text-muted">Random feature</a>
-            </li>
-            <li>
-              <a className="text-muted">Team feature</a>
-            </li>
-            <li>
-              <a className="text-muted">Stuff for developers</a>
-            </li>
-            <li>
-              <a className="text-muted">Another one</a>
-            </li>
-            <li>
-              <a className="text-muted">Last time</a>
-            </li>
+          <ul className="list-unstyled text-small text-muted">
+            <li>Cool stuff</li>
+            <li>Random feature</li>
+            <li>Team feature</li>
+            <li>Stuff for developers</li>
+            <li>Another one</li>
+            <li>Last time</li>
           </ul>
         </div>
         <div className="col-6 col-md">
           <h5 className="text-white">Resources</h5>
-          <ul className="list-unstyled text-small">
-            <li>
-              <a className="text-muted">Resource</a>
-            </li>
-            <li>
-              <a className="text-muted">Resource name</a>
-            </li>
-            <li>
-              <a className="text-muted">Another resource</a>
-            </li>
-            <li>
-              <a className="text-muted">Final resource</a>
-            </li>
+          <ul className="list-unstyled text-small text-muted">
+            <li>Resource</li>
+            <li>Resource name</li>
+            <li>Another resource</li>
+            <li>Final resource</li>
           </ul>
         </div>
         <div className="col-6 col-md">
           <h5 className="text-white">Resources</h5>
-          <ul className="list-unstyled text-small">
-            <li>
-              <a className="text-muted">Business</a>
-            </li>
-            <li>
-              <a className="text-muted">Education</a>
-            </li>
-            <li>
-              <a className="text-muted">Government</a>
-            </li>
-            <li>
-              <a className="text-muted">Gaming</a>
-            </li>
+          <ul className="list-unstyled text-small text-muted">
+            <li>Business</li>
+            <li>Education</li>
+            <li>Government</li>
+            <li>Gaming</li>
           </ul>
         </div>
         <div className="col-6 col-md">
           <h5 className="text-white">About</h5>
-          <ul className="list-unstyled text-small">
-            <li>
-              <a className="text-muted">Team</a>
-            </li>
-            <li>
-              <a className="text-muted">Locations</a>
-            </li>
-            <li>
-              <a className="text-muted">Privacy</a>
-            </li>
-            <li>
-              <a className="text-muted">Terms</a>
-            </li>
+          <ul className="list-unstyled text-small text-muted">
+            <li>Team</li>
+            <li>Locations</li>
+            <li>Privacy</li>
+            <li>Terms</li>
           </ul>
         </div>
       </div>
