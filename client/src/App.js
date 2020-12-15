7a297b4453a3b8004bf1b210a419fbb7cf71f559
Faris Chtatou <faris.manage@gmail.com>
2020-12-16 00:23:52 +0100

    prettier commit + new page shop

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,34 @@
-import React, { useEffect } from "react";
-import { Switch, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import ReactGa from "react-ga";
+import React, { useEffect } from "react"
+import { Switch, Route } from "react-router-dom"
+import { ToastContainer } from "react-toastify"
+import ReactGa from "react-ga"
 
-import "react-toastify/dist/ReactToastify.css";
+import "react-toastify/dist/ReactToastify.css"
 
-import Home from "./pages/Home/Home";
-import Register from "./pages/Register/Register";
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer/Footer";
-import UserDashboard from "./pages/User/UserDashboard";
-import UserRoute from "./routes/UserRoute";
-import AdminDashboard from "./pages/Admin/AdminDashboard";
-import AdminRoute from "./routes/AdminRoute";
-import ProductView from "./pages/Product/ProductView";
-import Error404 from "./pages/404/Error404";
+import Home from "./pages/Home/Home"
+import Register from "./pages/Register/Register"
+import NavBar from "./components/NavBar"
+import Footer from "./components/Footer/Footer"
+import UserDashboard from "./pages/User/UserDashboard"
+import UserRoute from "./routes/UserRoute"
+import AdminDashboard from "./pages/Admin/AdminDashboard"
+import AdminRoute from "./routes/AdminRoute"
+import ProductView from "./pages/Product/ProductView"
+import ShopContainer from "./pages/Shop/ShopContainer"
+import Error404 from "./pages/404/Error404"
 
-import { currentUser } from "./redux";
-import { connect } from "react-redux";
+import { currentUser } from "./redux"
+import { connect } from "react-redux"
 
 const App = ({ currentUser }) => {
   ReactGa.initialize(process.env.REACT_APP_GA_ID, {
     cookieDomain: "auto",
     debug: false,
-  });
+  })
 
   useEffect(() => {
-    currentUser();
-  }, [currentUser]);
+    currentUser()
+  }, [currentUser])
 
   return (
     <React.Fragment>
@@ -38,6 +39,7 @@ const App = ({ currentUser }) => {
           <Route component={Home} path="/" exact />
           <Route component={Register} path="/register" exact />
           <Route component={ProductView} path="/product/:slug" exact />
+          <Route component={ShopContainer} path="/shop" exact />
           <UserRoute component={UserDashboard} path="/user/dashboard" />
           <AdminRoute component={AdminDashboard} path="/admin/dashboard" />
           <Route component={Error404} />
@@ -45,19 +47,19 @@ const App = ({ currentUser }) => {
       </div>
       <Footer />
     </React.Fragment>
-  );
-};
+  )
+}
 
 const mapStateToProps = (state) => {
   return {
     userData: state.user,
-  };
-};
+  }
+}
 
 const mapDispatchToProps = (dispatch) => {
   return {
     currentUser: () => dispatch(currentUser()),
-  };
-};
+  }
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App)
