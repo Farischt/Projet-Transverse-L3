72f0a324e2951860b366d51a6397d65a8f7d2dea
Faris Chtatou <faris.manage@gmail.com>
2020-12-16 00:29:39 +0100

    new component that render products and searched products with a sidebar that handle search

---
+++ client/src/pages/Shop/ShopContainer.jsx
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from "react"
+import { connect } from "react-redux"
+import { searchWithQuery } from "../../redux"
+import { listProducts } from "../../api/product"
+import CardColumns from "react-bootstrap/CardColumns"
+import Spinner from "react-bootstrap/Spinner"
+import ProductCard from "../Home/components/ProductCard"
+import ProductLoading from "../Home/components/ProductLoading"
+import SideBar from "./components/SideBar"
+
+const ShopContainer = ({ searchData }) => {
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setLoading(true)
+      try {
+        const response = await listProducts()
+        setProducts(response.data)
+        setLoading(false)
+      } catch (err) {
+        setLoading(false)
+        console.log(err)
+      }
+    }
+    fetchProducts()
+  }, [])
+
+  return (
+    <div className="container-fluid">
+      <div className="row" style={{ minHeight: "100vh" }}>
+        <div className="col-md-2 p-3 bg-second">
+          <h6 className="text-white"> Menu de recherche </h6>
+          <hr />
+          <SideBar />{" "}
+        </div>
+        <div className="col-md-10 rounded pt-3">
+          {searchData && searchData.loading ? (
+            <div className="text-center">
+              <Spinner
+                animation="border"
+                variant="info"
+                style={{ marginTop: "10vh" }}
+              />
+              <CardColumns>
+                {" "}
+                <ProductLoading count={3} />{" "}
+              </CardColumns>
+            </div>
+          ) : searchData &&
+            searchData.products &&
+            searchData.products.length > 0 ? (
+            <>
+              <div className="jumbotron text-dark text-center h1 font-weight-bold">
+                <h3 className="display-4 text-danger">
+                  Votre recherche:{" "}
+                  {searchData.type === "price" ? (
+                    <>
+                      prix compris entre {searchData.query[0]}€ et{" "}
+                      {searchData.query[1]}€{" "}
+                    </>
+                  ) : searchData.type === "rating" ? (
+                    <> formation noté {searchData.query}/5 </>
+                  ) : (
+                    <> {searchData.query} </>
+                  )}
+                </h3>
+              </div>
+              <CardColumns>
+                {searchData.products.map((product) => {
+                  return <ProductCard key={product._id} product={product} />
+                })}
+              </CardColumns>
+              <hr />
+            </>
+          ) : searchData && searchData.error !== "" ? (
+            <div className="jumbotron text-dark text-center h1 font-weight-bold">
+              <h1 className="display-4 text-danger">{searchData.error} </h1>
+            </div>
+          ) : (
+            <div className="jumbotron text-dark h1 font-weight-bold">
+              <h1 className="display-4">Faites votre propre recherche ! </h1>
+              <p className="text-justify lead">
+                {" "}
+                Bienvenue dans la page d'achat et de recherche. Pour rechercher
+                un produit, séléctionnez un filtre dans la barre latérale
+                gauche, ou saisissez un mot clef dans la barre de recherche.
+              </p>
+            </div>
+          )}
+          <>
+            <h4 className="text-center text-info font-weight-bold">
+              {" "}
+              Notre séléction de produit{" "}
+            </h4>
+            <CardColumns>
+              {loading ? (
+                <ProductLoading count={3} />
+              ) : products && products.length > 0 ? (
+                products.map((product) => {
+                  return <ProductCard key={product._id} product={product} />
+                })
+              ) : (
+                <h1 className="text-danger"> Error fetching products</h1>
+              )}
+            </CardColumns>
+          </>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    searchData: state.search,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    searchWithQuery: (keyword, type) =>
+      dispatch(searchWithQuery(keyword, type)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShopContainer)
