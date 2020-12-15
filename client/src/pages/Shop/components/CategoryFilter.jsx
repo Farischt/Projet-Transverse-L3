6012b05798999ad2a734e15c7b096510585d53fa
Faris Chtatou <faris.manage@gmail.com>
2020-12-16 00:30:03 +0100

    new component: sidebar category filter

---
+++ client/src/pages/Shop/components/CategoryFilter.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react"
+import { connect } from "react-redux"
+import { searchWithQuery } from "../../../redux"
+import { getCategories } from "../../../api/category"
+import Spinner from "react-bootstrap/Spinner"
+
+const CategoryFilter = ({ searchData, searchWithQuery }) => {
+  const [categories, setCategories] = useState([])
+  const [categoriesLoading, setCategoriesLoading] = useState(false)
+
+  const fetchCategories = async () => {
+    setCategoriesLoading(true)
+    try {
+      const response = await getCategories()
+      setCategories(response.data)
+      setCategoriesLoading(false)
+    } catch (err) {
+      setCategoriesLoading(false)
+      if (err.response.status === 400) {
+        setCategories([])
+      } else setCategoriesLoading(false)
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    fetchCategories()
+  }, [])
+
+  const handleSelectChange = (e) => {
+    searchWithQuery(e.target.value, "category")
+  }
+
+  return (
+    <div className="ml-4 mr-4 p-2">
+      {categoriesLoading ? (
+        <Spinner animation="border" variant="info" />
+      ) : (
+        categories &&
+        categories.length > 0 && (
+          <select
+            className="custom-select custom-select-sm"
+            onChange={handleSelectChange}
+            defaultValue={{ value: "default" }}
+          >
+            <option value="default"> Choisir une catégorie </option>
+            {categories.map((category) => {
+              return (
+                <option key={category._id} value={category._id}>
+                  {" "}
+                  {category.name}{" "}
+                </option>
+              )
+            })}
+          </select>
+        )
+      )}
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    searchData: state.search,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    searchWithQuery: (filter, type) => dispatch(searchWithQuery(filter, type)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryFilter)
