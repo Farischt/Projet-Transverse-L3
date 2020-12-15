3ccdc0d7e872ffd68bb0d979da7ff1f239e3b0c4
Faris Chtatou <faris.manage@gmail.com>
2020-12-16 00:24:17 +0100

    prettier + imported custom style

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "antd/dist/antd.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from "react"
+import ReactDOM from "react-dom"
+import "antd/dist/antd.css"
+// import "bootstrap/dist/css/bootstrap.min.css"
+import "./custom.scss"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
 
-import { BrowserRouter } from "react-router-dom";
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
-import rootReducer from "./redux";
+import { BrowserRouter } from "react-router-dom"
+import { createStore, applyMiddleware } from "redux"
+import { Provider } from "react-redux"
+import { composeWithDevTools } from "redux-devtools-extension"
+import thunk from "redux-thunk"
+import rootReducer from "./redux"
 
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
-);
+)
 
 ReactDOM.render(
   <Provider store={store}>
@@ -25,9 +26,9 @@ ReactDOM.render(
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
