5afa1bd3169f16dffb2912cd54ee40a4d0c695a3
Faris Chtatou <faris.manage@gmail.com>
2020-12-16 00:25:37 +0100

    refactored component + prettier

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,75 +1,106 @@
-import React, { useState } from "react";
-import ReactGa from "react-ga";
-import { connect } from "react-redux";
-import { loginUser } from "../redux";
-import { Link } from "react-router-dom";
+import React, { useState } from "react"
+import ReactGa from "react-ga"
+import { connect } from "react-redux"
+import { useHistory } from "react-router-dom"
+import { loginUser } from "../redux"
+import Form from "react-bootstrap/Form"
+import FormControl from "react-bootstrap/FormControl"
+import Button from "react-bootstrap/Button"
 
 const Login = ({ userData, loginUser }) => {
+  let history = useHistory()
+
   const [user, setUser] = useState({
     email: "",
     password: "",
-  });
+  })
 
   const handleInputChange = (event) => {
     const userInfo = {
       ...user,
       [event.target.name]: event.target.value,
-    };
-    setUser(() => userInfo);
-  };
+    }
+    setUser(() => userInfo)
+  }
 
   const handleLogin = () => {
-    loginUser(user);
+    loginUser(user)
     ReactGa.event({
       category: "User",
       action: "User logged-in",
-    });
-  };
+    })
+  }
 
   return (
-    <div className="form-inline mt-2 mt-md-0">
-      <div>
-        <input
-          onChange={handleInputChange}
-          className="form-control mr-sm-2"
-          type="email"
-          name="email"
-          placeholder="Email"
-        />
-        <input
-          onChange={handleInputChange}
-          className="form-control my-2 mr-sm-2"
-          type="password"
-          name="password"
-          placeholder="Mot de passe"
-        />
-        <button
-          onClick={handleLogin}
-          className="btn btn-outline-info my-2 my-sm-0"
-        >
-          Se connecter
-        </button>
-        <Link to="/register" className="m-2">
-          <button className="btn btn-outline-info my-2 my-sm-0">
-            {" "}
-            S'inscrire{" "}
-          </button>
-        </Link>
-      </div>
-    </div>
-  );
-};
+    <Form inline onSubmit={handleLogin}>
+      <FormControl
+        onChange={handleInputChange}
+        type="email"
+        name="email"
+        placeholder="Email"
+        className="mr-sm-2 my-2"
+        style={{ maxWidth: "10rem" }}
+      />
+      <FormControl
+        onChange={handleInputChange}
+        type="password"
+        name="password"
+        placeholder="Mot de passe"
+        className="mr-sm-2 my-2"
+        style={{ maxWidth: "10rem" }}
+      />
+      <Button variant="outline-info" type="submit" className="mr-sm-2 my-2">
+        Se connecter
+      </Button>
+      <Button variant="outline-info" onClick={() => history.push("/register")}>
+        S'inscrire
+      </Button>
+    </Form>
+    // <div className="form-inline mt-2 mt-md-0">
+    //   {/* <div className="form-inline"> */}
+    //   <input
+    //     onChange={handleInputChange}
+    //     className="form-control my-2 mr-sm-2"
+    //     type="email"
+    //     name="email"
+    //     placeholder="Email"
+    //     style={{ maxWidth: "12rem" }}
+    //   />
+    //   <input
+    //     onChange={handleInputChange}
+    //     className="form-control my-2 mr-sm-2 ml-2"
+    //     type="password"
+    //     name="password"
+    //     placeholder="Mot de passe"
+    //     style={{ maxWidth: "12rem" }}
+    //   />
+    //   <button
+    //     onClick={handleLogin}
+    //     className="btn btn-outline-info my-2 my-sm-0"
+    //   >
+    //     Se connecter
+    //   </button>
+    //   <Link to="/register" className="m-2">
+    //     <button className="btn btn-outline-info my-2 my-sm-0">
+    //       {" "}
+    //       S'inscrire{" "}
+    //     </button>
+    //   </Link>
+    // </div>
+    // </div>
+  )
+}
 
 const mapStateToProps = (state) => {
   return {
     userData: state.user,
-  };
-};
+  }
+}
 
 const mapDispatchToProps = (dispatch) => {
   return {
     loginUser: (user) => dispatch(loginUser(user)),
-  };
-};
+  }
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
