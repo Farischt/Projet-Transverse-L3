c1834cf9d04fe5001cd21d7bfcd64f145346b7fc
Faris Chtatou <faris.manage@gmail.com>
2020-12-16 17:28:00 +0100

    added search input in sidebar

--- client/src/pages/Shop/components/SideBar.jsx
+++ client/src/pages/Shop/components/SideBar.jsx
@@ -2,19 +2,20 @@ import React from "react"
 import PriceFilter from "./PriceFilter"
 import CategoryFilter from "./CategoryFilter"
 import StarFilter from "./StarFilter"
+import SearchBar from "../../../components/SearchBar"
 import { Menu } from "antd"
 const { SubMenu } = Menu
 
 const SideBar = () => {
   return (
     <Menu
-      defaultOpenKeys={["sub1", "sub2", "sub3"]}
+      defaultOpenKeys={["sub1", "sub2", "sub3", "sub4"]}
       mode="inline"
       className="rounded"
-      theme="light"
+      theme="dark"
     >
       <SubMenu key="sub1" title="Filtre par note" className=" rounded">
-        <div className="pr-4 pl-4 pb-2">
+        <div className="pr-4 pl-4 py-2">
           <StarFilter />
         </div>
       </SubMenu>
@@ -23,6 +24,11 @@ const SideBar = () => {
       </SubMenu>
       <SubMenu key="sub3" title="Filtre par catégorie">
         <CategoryFilter />
+      </SubMenu>
+      <SubMenu key="sub4" title="Filtre par mot clef">
+        <div className="px-2 py-2">
+          <SearchBar />
+        </div>
       </SubMenu>
     </Menu>
   )
