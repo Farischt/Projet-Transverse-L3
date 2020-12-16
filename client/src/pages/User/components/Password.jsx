9d0bb3c1d10d42310b0748a8b77a8a3435d60692
Faris Chtatou <faris.manage@gmail.com>
2020-12-16 17:28:25 +0100

    prettier + main button bgcolor second

--- client/src/pages/User/components/Password.jsx
+++ client/src/pages/User/components/Password.jsx
@@ -1,37 +1,37 @@
-import React, { useState } from "react";
-import { toast } from "react-toastify";
-import axios from "axios";
+import React, { useState } from "react"
+import { toast } from "react-toastify"
+import axios from "axios"
 
 const Password = () => {
   const [userInfo, setUserInfo] = useState({
     currentPassword: "",
     newPassword: "",
     repeatPassword: "",
-  });
+  })
 
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false)
 
   const handleInputChange = (event) => {
     const userInput = {
       ...userInfo,
       [event.target.name]: event.target.value,
-    };
-    setUserInfo(() => userInput);
-  };
+    }
+    setUserInfo(() => userInput)
+  }
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
     try {
-      setIsLoading(true);
-      await axios.put("/api/user/password", userInfo);
-      setIsLoading(false);
-      toast.success(" Votre mot de passe a été modifié ");
+      setIsLoading(true)
+      await axios.put("/api/user/password", userInfo)
+      setIsLoading(false)
+      toast.success(" Votre mot de passe a été modifié ")
     } catch (err) {
-      setIsLoading(false);
-      console.log(err);
-      toast.error(" Une erreur est survenu !");
+      setIsLoading(false)
+      console.log(err)
+      toast.error(" Une erreur est survenu !")
     }
-  };
+  }
 
   return (
     <div className="col p-4 bg-light rounded" style={{ minHeight: "90vh" }}>
@@ -73,7 +73,7 @@ const Password = () => {
           </div>
           <div className="form-row">
             <div className="form-group col">
-              <button className="btn btn-info my-2 my-sm-0" type="submit">
+              <button className="btn btn-second my-2 my-sm-0" type="submit">
                 {" "}
                 Modifier le mot de passe{" "}
               </button>
@@ -88,7 +88,7 @@ const Password = () => {
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default Password;
+export default Password
