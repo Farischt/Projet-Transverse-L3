9d9bdf99b9498a1a4ef21448d465a61022c27e7a
Faris Chtatou <faris.manage@gmail.com>
2020-12-16 17:25:19 +0100

    padding + bground color

--- client/src/pages/Admin/AdminDashboard.jsx
+++ client/src/pages/Admin/AdminDashboard.jsx
@@ -1,22 +1,27 @@
-import React from "react";
-import { Switch, Route, useRouteMatch } from "react-router-dom";
+import React from "react"
+import { Switch, Route, useRouteMatch } from "react-router-dom"
 
-import AdminNav from "../../components/Nav/AdminNav";
-import CategoryContainer from "./Category/CategoryContainer";
-import ProductContainer from "./Product/ProductContainer";
+import AdminNav from "../../components/Nav/AdminNav"
+import CategoryContainer from "./Category/CategoryContainer"
+import ProductContainer from "./Product/ProductContainer"
 
 const AdminDashboard = () => {
-  let { path, url } = useRouteMatch();
+  let { path, url } = useRouteMatch()
   return (
     <div className="overflow-hidden">
-      <div className="row bg-dark">
-        <div className="col-md-2 bg-dark">
+      <div className="row bg-main">
+        <div className="col-md-2">
           <AdminNav url={url} />
         </div>
         <div className="col-md-10 ">
           <Switch>
             <Route path={`${path}`} exact>
-              Votre Dashboard Administrateur
+              <div
+                className="col p-4 bg-light rounded"
+                style={{ minHeight: "90vh" }}
+              >
+                Votre compte
+              </div>
             </Route>
             <Route component={CategoryContainer} path={`${path}/categories`} />
             <Route component={ProductContainer} path={`${path}/products`} />
@@ -24,7 +29,7 @@ const AdminDashboard = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default AdminDashboard;
+export default AdminDashboard
--- client/src/pages/User/UserDashboard.jsx
+++ client/src/pages/User/UserDashboard.jsx
@@ -1,28 +1,33 @@
-import React from "react";
-import { Switch, Route, useRouteMatch } from "react-router-dom";
-import UserNav from "../../components/UserNav";
-import Password from "./components/Password";
+import React from "react"
+import { Switch, Route, useRouteMatch } from "react-router-dom"
+import UserNav from "../../components/UserNav"
+import Password from "./components/Password"
 
 const UserDashboard = () => {
-  let { path, url } = useRouteMatch();
+  let { path, url } = useRouteMatch()
 
   return (
     <div className="overflow-hidden">
-      <div className="row bg-dark">
-        <div className="col-md-2 bg-dark">
+      <div className="row bg-main">
+        <div className="col-md-2 ">
           <UserNav url={url} />
         </div>
         <div className="col-md-10">
           <Switch>
             <Route path={`${path}`} exact>
-              Votre Espace Personnel
+              <div
+                className="col p-4 bg-light rounded"
+                style={{ minHeight: "90vh" }}
+              >
+                Votre compte
+              </div>
             </Route>
             <Route component={Password} path={`${path}/password`} />
           </Switch>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default UserDashboard;
+export default UserDashboard
