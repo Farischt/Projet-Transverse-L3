ab091df625d2f9cb4557bb2f54f65eb3b6454a6b
Faris Chtatou <faris.manage@gmail.com>
2020-12-21 01:14:09 +0100

    prettier ext

--- client/src/pages/Product/ProductListDetails.jsx
+++ client/src/pages/Product/ProductListDetails.jsx
@@ -1,34 +1,32 @@
-import React from "react";
+import React from "react"
 
 const ProductListDetails = ({ product }) => {
   return (
     <ul className="list-group">
-      <li className="list-group-item">
+      <li className="list-group-item d-flex justify-content-between align-items-center px-3">
         Prix
-        <span className=" float-sm-right">
+        <strong>
           {product &&
             new Intl.NumberFormat("de-DE", {
               style: "currency",
               currency: "EUR",
             }).format(product.price)}
-        </span>
+        </strong>
       </li>
-      <li className="list-group-item">
+      <li className="list-group-item d-flex justify-content-between align-items-center px-3">
         Catégorie
-        <span className=" float-sm-right">
-          {product && product.category && product.category.name}
-        </span>
+        <strong>{product && product.category && product.category.name}</strong>
       </li>
-      <li className="list-group-item">
+      <li className="list-group-item d-flex justify-content-between align-items-center px-3">
         Quantité
-        <span className=" float-sm-right">{product && product.quantity}</span>
+        <strong>{product && product.quantity}</strong>
       </li>
-      <li className="list-group-item">
+      <li className="list-group-item d-flex justify-content-between align-items-center px-3">
         Vendu
-        <span className=" float-sm-right">{product && product.sold}</span>
+        <strong>{product && product.sold}</strong>
       </li>
     </ul>
-  );
-};
+  )
+}
 
-export default ProductListDetails;
+export default ProductListDetails
