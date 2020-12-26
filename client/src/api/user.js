032959a364ebefc39ac3fcf8785cb8d6c0d52f61
Faris Chtatou <faris.manage@gmail.com>
2020-12-26 01:51:27 +0100

    changed endpoint

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const userCart = async (cart) => {
-  return await axios.post(`${process.env.REACT_APP_API_URL}/user/cart`, {
+  return await axios.post(`${process.env.REACT_APP_API_URL}/user-action/cart`, {
     cart,
   })
 }
