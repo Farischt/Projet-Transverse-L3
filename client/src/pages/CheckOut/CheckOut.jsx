70b3ff4f9096e6046ca47c9c12571f741df1347f
Faris Chtatou <faris.manage@gmail.com>
2020-12-26 01:18:00 +0100

    user checkout page first commit

---
+++ client/src/pages/CheckOut/CheckOut.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const CheckOut = () => {
+  return (
+    <div className="container-fluid" style={{ minHeight: "80vh" }}>
+      <div className="row">
+        <div className="col-md-6">Hey</div>
+        <div className="col-md-6">Welcome</div>
+      </div>
+    </div>
+  )
+}
+
+export default CheckOut
