2f01bdeccd7bdebfe4a4a8d6764128d71f3f1536
Faris Chtatou <faris.manage@gmail.com>
2020-12-26 01:17:36 +0100

    imported userRoute

--- server/index.js
+++ server/index.js
@@ -1,37 +1,37 @@
 // Dependencies
-const express = require("express");
-const session = require("express-session");
-const logger = require("morgan");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
+const express = require("express")
+const session = require("express-session")
+const logger = require("morgan")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
 // Routes
-const authRoute = require("./routes/auth");
-const categoryRoute = require("./routes/category");
-const productRoute = require("./routes/product");
-const cloudinaryRoute = require("./routes/cloudinary");
-const connectDb = require("./database");
+const authRoute = require("./routes/auth")
+const categoryRoute = require("./routes/category")
+const productRoute = require("./routes/product")
+const cloudinaryRoute = require("./routes/cloudinary")
+const userRoute = require("./routes/user")
+const connectDb = require("./database")
 
-const app = express();
-connectDb();
+const app = express()
+connectDb()
 //Middlewares
-app.use(logger("dev"));
-app.use(express.json({ limit: "2mb" }));
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger("dev"))
+app.use(express.json({ limit: "2mb" }))
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     saveUninitialized: false,
     resave: false,
   })
-);
-app.use(cors());
+)
+app.use(cors())
 // Route Middlewares :
-app.use("/api/user", authRoute);
-app.use("/api", categoryRoute);
-app.use("/api", productRoute);
-app.use("/api/", cloudinaryRoute);
+app.use("/api/user", authRoute)
+app.use("/api", categoryRoute)
+app.use("/api", productRoute)
+app.use("/api/", cloudinaryRoute)
+app.use("/api/usercart/", userRoute)
 
-app.listen(process.env.PORT || 3080, () =>
-  console.log("Server up and running")
-);
+app.listen(process.env.PORT || 3080, () => console.log("Server up and running"))
