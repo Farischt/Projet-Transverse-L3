a2fec0ae41da26fc5ec276006f3140dd10857cef
Faris Chtatou <faris.manage@gmail.com>
2020-12-30 23:36:13 +0100

    new routes to get and delete user cart

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,18 @@
 const router = require("express").Router()
 // Middlewares
-const { verifyAuth, verifyAdmin } = require("../middlewares/auth")
+const { verifyAuth } = require("../middlewares/auth")
 // Controllers
-const { userCart } = require("../controller/userController")
+const {
+  userCart,
+  readCart,
+  deleteCart,
+} = require("../controller/userController")
 
+// Route that save user cart in database
 router.post("/cart", verifyAuth, userCart)
+// Route that get actual user cart
+router.get("/cart", verifyAuth, readCart)
+// Route that delete a cart
+router.delete("/cart", verifyAuth, deleteCart)
 
 module.exports = router
