1e9dbbed731434fc196310ab40f5c067b95ef7bb
Faris Chtatou <faris.manage@gmail.com>
2021-01-05 00:18:59 +0100

    apply coupon api call

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -13,3 +13,10 @@ export const getUserCart = async () => {
 export const deleteUserCart = async () => {
   return await axios.delete(`${process.env.REACT_APP_API_URL}/user-action/cart`)
 }
+
+export const applyCoupon = async (coupon) => {
+  return await axios.post(
+    `${process.env.REACT_APP_API_URL}/user-action/cart/apply-coupon`,
+    { coupon }
+  )
+}
