698790223a720bed33ecd23e5b34717d701d5425
Faris Chtatou <faris.manage@gmail.com>
2021-01-09 23:04:57 +0100

    component that show product details

---
+++ client/src/pages/Admin/Home/OrderProductDetails.jsx
@@ -0,0 +1,40 @@
+import React from "react"
+
+const OrderProductDetails = ({ order }) => {
+  return (
+    <div className="table-responsive p-2">
+      <h3 className="pb-2"> Produits </h3>
+      <table className="table table-bordered table-hover ">
+        <thead className="thead-light">
+          <tr>
+            <th scope="col"> Nom </th>
+            <th scope="col"> Quantité </th>
+            <th scope="col"> Prix unitaire </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {order.products.map((element, index) => {
+            return (
+              <tr key={index}>
+                <td>
+                  {" "}
+                  <b> {element.product.name} </b>{" "}
+                </td>
+                <td>{element.userQuantity}</td>
+                <td>
+                  {new Intl.NumberFormat("de-DE", {
+                    style: "currency",
+                    currency: "EUR",
+                  }).format(element.product.price)}
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default OrderProductDetails
