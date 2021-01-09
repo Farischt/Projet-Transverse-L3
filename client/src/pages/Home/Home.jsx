3232b8e57cbce5b31c712006f1e4d15b6606c745
Faris Chtatou <faris.manage@gmail.com>
2021-01-09 23:06:39 +0100

    modified jumbotron text, and deleted a divider

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react"
+
 import ReactGa from "react-ga"
 import Jumbotron from "./components/Jumbotron"
 import NewArrivals from "./components/NewArrivals"
@@ -11,13 +12,16 @@ const Home = () => {
 
   return (
     <>
-      <div className="jumbotron text-center text-main h1 font-weight-bold">
+      <div
+        className="jumbotron text-center text-main display-3"
+        style={{ borderRadius: "0px" }}
+      >
         <Jumbotron
           text={[
+            "Bonjour !",
             "Bienvenue sur Formations & Logiciels",
             "Besoin d'une formation dynamique ?",
-            "Vous êtes au bon endroit !",
-            "Ne manquez pas nos nouvelles formations !",
+            "Tu es au bon endroit !",
           ]}
         />
       </div>
@@ -29,9 +33,6 @@ const Home = () => {
         <hr />
       </div>
       <BestSellers />
-      <div className="container">
-        <hr />
-      </div>
     </>
   )
 }
