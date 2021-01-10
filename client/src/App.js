a9ccc70b36ffb45167f90fd36df3b232d69864bc
Faris Chtatou <faris.manage@gmail.com>
2021-01-10 21:39:12 +0100

    react lazy and suspence + sideDrawer

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,27 @@
-import React, { useEffect } from "react"
+import React, { useEffect, lazy, Suspense } from "react"
 import { Switch, Route } from "react-router-dom"
 import { ToastContainer } from "react-toastify"
+import Spinner from "react-bootstrap/Spinner"
 import ReactGa from "react-ga"
-
-import "react-toastify/dist/ReactToastify.css"
-
-import Home from "./pages/Home/Home"
-import Register from "./pages/Register/Register"
-import NavBar from "./components/NavBar"
-import Footer from "./components/Footer/Footer"
-import UserDashboard from "./pages/User/UserDashboard"
-import UserRoute from "./routes/UserRoute"
-import AdminDashboard from "./pages/Admin/AdminDashboard"
-import AdminRoute from "./routes/AdminRoute"
-import ProductView from "./pages/Product/ProductView"
-import ShopContainer from "./pages/Shop/ShopContainer"
-import CartView from "./pages/Cart/CartView"
-import CheckOut from "./pages/CheckOut/CheckOut"
-import Payment from "./pages/Payment/Payment"
-import Error404 from "./pages/404/Error404"
-
 import { currentUser } from "./redux"
 import { connect } from "react-redux"
+import "react-toastify/dist/ReactToastify.css"
+
+const Home = lazy(() => import("./pages/Home/Home"))
+const Register = lazy(() => import("./pages/Register/Register"))
+const NavBar = lazy(() => import("./components/NavBar"))
+const Footer = lazy(() => import("./components/Footer/Footer"))
+const UserDashboard = lazy(() => import("./pages/User/UserDashboard"))
+const UserRoute = lazy(() => import("./routes/UserRoute"))
+const AdminDashboard = lazy(() => import("./pages/Admin/AdminDashboard"))
+const AdminRoute = lazy(() => import("./routes/AdminRoute"))
+const ProductView = lazy(() => import("./pages/Product/ProductView"))
+const ShopContainer = lazy(() => import("./pages/Shop/ShopContainer"))
+const CartView = lazy(() => import("./pages/Cart/CartView"))
+const CheckOut = lazy(() => import("./pages/CheckOut/CheckOut"))
+const Payment = lazy(() => import("./pages/Payment/Payment"))
+const Error404 = lazy(() => import("./pages/404/Error404"))
+const SideDrawer = lazy(() => import("./components/SideDrawer"))
 
 const App = ({ currentUser }) => {
   ReactGa.initialize(process.env.REACT_APP_GA_ID, {
@@ -34,8 +34,20 @@ const App = ({ currentUser }) => {
   }, [currentUser])
 
   return (
-    <React.Fragment>
+    <Suspense
+      fallback={
+        <div className="text-center p-5">
+          {" "}
+          <h1>
+            {" "}
+            Chargement... <br />
+            <Spinner animation="border" variant="dark" />{" "}
+          </h1>{" "}
+        </div>
+      }
+    >
       <NavBar />
+      <SideDrawer />
       <ToastContainer newestOnTop={false} />
       <div>
         <Switch>
@@ -52,7 +64,7 @@ const App = ({ currentUser }) => {
         </Switch>
       </div>
       <Footer />
-    </React.Fragment>
+    </Suspense>
   )
 }
 
