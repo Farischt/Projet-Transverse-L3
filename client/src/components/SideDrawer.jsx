641fb946312b2e2be40318d910a49a2bfc22989f
Faris Chtatou <faris.manage@gmail.com>
2021-01-11 13:17:24 +0100

    side drawer done

--- client/src/components/SideDrawer.jsx
+++ client/src/components/SideDrawer.jsx
@@ -1,20 +1,57 @@
 import React from "react"
 import { connect } from "react-redux"
-import { setDrawerVisible, setDrawerHidden } from "../redux"
+import { useHistory } from "react-router-dom"
+import { setDrawerHidden } from "../redux"
 import { Drawer } from "antd"
 
 const SideDrawer = ({ cartData, drawerData, setDrawerHidden }) => {
+  let history = useHistory()
+
+  const redirectToCart = () => {
+    if (history.location.pathname !== "/cart") {
+      history.push("/cart")
+      setDrawerHidden()
+    }
+  }
+
   return (
     <Drawer
       visible={drawerData.visibility}
-      onClose={setDrawerHidden}
+      onClose={(e) => {
+        e.preventDefault()
+        setDrawerHidden()
+      }}
+      getContainer={false}
       title={`Votre panier (${cartData.length})`}
+      closable={false}
     >
-      {" "}
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio dicta
-      repellendus voluptatum, nesciunt deleniti nostrum asperiores pariatur ex
-      laboriosam culpa ipsa sit assumenda laudantium incidunt reiciendis
-      reprehenderit, rem, quaerat esse?{" "}
+      <div className="row">
+        <div className="col">
+          <button className="btn btn-block btn-info" onClick={redirectToCart}>
+            {" "}
+            Vers votre panier{" "}
+          </button>
+          {cartData &&
+            cartData.map((product) => {
+              return (
+                <div className="border rounded mt-2" key={product._id}>
+                  <img
+                    className="img-fluid rounded-top"
+                    src={product.images[0].url}
+                    style={{ width: "100%", objectFit: "cover" }}
+                  />
+                  <li className="list-group-item d-flex justify-content-between align-items-center px-3">
+                    <strong> {product.name} </strong> {product.userQuantity}
+                  </li>
+                  <button className="btn btn-block btn-danger">
+                    {" "}
+                    Retirer{" "}
+                  </button>
+                </div>
+              )
+            })}
+        </div>
+      </div>
     </Drawer>
   )
 }
