37bc143cf3f45fae16cb4f5424ef3b9cc499310c
Faris Chtatou <faris.manage@gmail.com>
2021-01-12 19:06:57 +0100

    sales per month, + out of stocks methods

--- server/controller/adminController.js
+++ server/controller/adminController.js
@@ -1,5 +1,6 @@
 const Order = require("../model/Order")
 const mongoose = require("mongoose")
+const Product = require("../model/Product")
 const ObjectId = mongoose.Types.ObjectId
 
 // ? get all orders
@@ -56,6 +57,71 @@ module.exports.orderStatus = async (req, res) => {
     res.json({ ok: true })
   } catch (err) {
     console.log(err)
+    return res.status(500).json({ errorMessage: err.message })
+  }
+}
+
+module.exports.grossSalesPerMonth = async (req, res) => {
+  const validStatus = ["Completed", "Not processed", "Processing"]
+
+  try {
+    const result = await Order.aggregate([
+      {
+        $match: {
+          orderStatus: { $in: validStatus },
+          createdAt: {
+            $gte: new Date("2021-01-01T00:00:00.000Z"), // Year 2021
+          },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            month: {
+              $month: {
+                $toDate: { $multiply: ["$paymentIntent.created", 1000] },
+              },
+            },
+            year: {
+              $year: {
+                $toDate: { $multiply: ["$paymentIntent.created", 1000] },
+              },
+            },
+          },
+          grossSales: { $sum: "$paymentIntent.amount" },
+        },
+      },
+      { $sort: { "paymentIntent.created": -1 } },
+    ])
+
+    if (!result.length)
+      return res
+        .status(400)
+        .json({ errorMessage: "No available data to sum your gross sales" })
+
+    res.json(result)
+  } catch (err) {
+    console.log(err.message)
+    return res.status(500).json({ errorMessage: err.message })
+  }
+}
+
+module.exports.outOfStock = async (req, res) => {
+  try {
+    const soonOutOfStocks = await Product.find({
+      quantity: { $lte: 10 },
+    })
+      .sort({ quantity: 1 })
+      .limit(10)
+
+    if (!soonOutOfStocks.length)
+      return res
+        .status(400)
+        .json({ errorMessage: "No products are soon out of stocks" })
+
+    res.json(soonOutOfStocks)
+  } catch (err) {
+    console.log(err.message)
     return res.status(500).json({ errorMessage: err.message })
   }
 }
