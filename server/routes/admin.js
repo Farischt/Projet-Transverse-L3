a994b153775818f3066b85674b5ca337c1562703
Faris Chtatou <faris.manage@gmail.com>
2021-01-12 19:07:23 +0100

    new routes to handle sales and stocks requests

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,11 +2,20 @@ const router = require("express").Router()
 // Middlewares
 const { verifyAuth, verifyAdmin } = require("../middlewares/auth")
 // Controllers
-const { orders, orderStatus } = require("../controller/adminController")
+const {
+  orders,
+  orderStatus,
+  grossSalesPerMonth,
+  outOfStock,
+} = require("../controller/adminController")
 
 // Route that get all the orders from db
 router.get("/orders", verifyAuth, verifyAdmin, orders)
 // Routes that update an order status
 router.put("/orders/:orderId", verifyAuth, verifyAdmin, orderStatus)
+// Routes thath get sales per month for 2021 year
+router.get("/sales-per-month", verifyAuth, verifyAdmin, grossSalesPerMonth)
+// Routes that get all products soon out of stocks
+router.get("/out-of-stocks", verifyAuth, verifyAdmin, outOfStock)
 
 module.exports = router
