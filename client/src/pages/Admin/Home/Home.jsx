a3ff75e7d39c3de486eda22542026e35357d3c5e
Faris Chtatou <faris.manage@gmail.com>
2021-01-12 19:05:22 +0100

    updated home page to show graphs

--- client/src/pages/Admin/Home/Home.jsx
+++ client/src/pages/Admin/Home/Home.jsx
@@ -1,13 +1,18 @@
-import React, { useState, useEffect } from "react"
-import Spinner from "react-bootstrap/Spinner"
+import React from "react"
+import GrossSales from "./GrossSales"
 import Sales from "./Sales"
+import Stocks from "./Stocks"
 
 const Home = () => {
   return (
     <div className="col p-4 bg-light rounded" style={{ minHeight: "90vh" }}>
       <h1> Votre tableau de bord </h1>
+      <hr />
+      <GrossSales />
+      <hr />
       <Sales />
       <hr />
+      <Stocks />
     </div>
   )
 }
